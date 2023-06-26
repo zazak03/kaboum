9273232da79b1894107e337c8aac241ec86e457f
JustAlternate <lololollololol890@gmail.com>
2023-06-26 19:47:50 +0200

    erzer

--- home.gd
+++ home.gd
@@ -44,6 +44,7 @@ func _on_button_pressed():
 func _on_reset_pressed():
 	number_of_barrel = $Ui/HSlider.value
 	number_of_random_debris = $Ui/HSlider2.value
+	
 	game_is_playing = false
 	var decalage = [0,0]
 	for old_barrel in $barrels.get_children():
@@ -59,6 +60,8 @@ func _on_reset_pressed():
 		if decalage[0]%150==0:
 			decalage[1]+=50
 			decalage[0]=0
+		
+		barrel.force = $Ui/HSlider3.value
 		get_node("barrels").add_child(barrel)
 	
 	for i in range(number_of_random_debris):
