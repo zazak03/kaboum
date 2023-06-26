1b7b7411798f1b64b3197f78bbe7678884fc330c
JustAlternate <lololollololol890@gmail.com>
2023-06-26 18:08:25 +0200

    home

---
+++ home.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var picked = false
+var barrel_position
+var radius
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	radius = 16
+	barrel_position = $StaticBody2D.position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if barrel_position.x - radius <= get_global_mouse_position().x and get_global_mouse_position().x <= barrel_position.x + radius and barrel_position.y - radius <= get_global_mouse_position().y and get_global_mouse_position().y <= barrel_position.y + radius :
+		print("inside")
+		if Input.is_action_just_pressed("click"):
+			picked = true
+			print("picked")
+	if picked:
+		if Input.is_action_just_released("click"):
+			picked = false
+		$StaticBody2D.position=get_global_mouse_position()
+
